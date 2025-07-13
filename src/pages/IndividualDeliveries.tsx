188046a2acd493dc08f7836e7c3a3499749653a6
Marsel-cyber <72985862+Marsel-cyber@users.noreply.github.com>
2025-07-13 10:20:51 +0700

    Updated IndividualDeliveries.tsx

--- src/pages/IndividualDeliveries.tsx
+++ src/pages/IndividualDeliveries.tsx
@@ -375,7 +375,7 @@ const IndividualDeliveries: React.FC = () => {
     <title>Faktur Penjualan - ${delivery.id}</title>
     <style>
       body {
-        font-family: "Courier New", Courier, monospace;
+        font-family: "Tahoma", sans-serif;
         font-size: 12px;
         margin: 40px;
         color: #000;
